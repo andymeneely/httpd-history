a2de4bc59a661b432b2d5fd8d4941c59ff7ad190
Andy Meneely <andy.meneely@gmail.com>
2012-09-05 00:30:55 -0400

    CVE-2003-0020 bisected, hopefully
    not quite sure if this one is accurate, but here goes

---
+++ src/main/java/edu/rit/se/history/httpd/intro/GitBisectReturnCVE20030020.java
@@ -0,0 +1,106 @@
+package edu.rit.se.history.httpd.intro;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * CVE-2003-0020: server/log.c
+ * 
+ * Fix commit: 88f261355a83c28097a18f9b15d92196ec290def
+ * 
+ * Origin commit: 5430f8800f5fffd57e7421dee0ac9de8ca4f9573
+ * 
+ * <pre>
+ *  git bisect start 88f261355a83c28097a18f9b15d92196ec290def^ 5430f8800f5fffd57e7421dee0ac9de8ca4f9573 -- server/log.c
+ *  git bisect run java -cp ../httpd-history/src/main/java/ edu.rit.se.history.httpd.intro.GitBisectReturnCVE20030020
+ * </pre>
+ * 
+ * @author Andy Meneely
+ * 
+ */
+public class GitBisectReturnCVE20030020 {
+
+	private static final int GOOD_RETURN_CODE = 0;
+	private static final int BAD_RETURN_CODE = 1;
+	private static final int SKIP_RETURN_CODE = 125;
+
+	private static final String CVE = "CVE-2003-0020";
+	private static final String FILE = "server/log.c";
+
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			System.out.println("No arguments allowed to this script!");
+			System.exit(SKIP_RETURN_CODE);
+		}
+		System.out.println("===Bisect check for " + CVE + ", " + FILE + "===");
+		try {
+			if (isVulnerable(FILE)) {
+				System.out.println("===VULNERABLE===");
+				System.exit(BAD_RETURN_CODE); // vulnerable --> commit was "bad" --> abnormal termination
+			} else {
+				System.out.println("===NEUTRAL===");
+				System.exit(GOOD_RETURN_CODE); // neutral --> commit was "good" --> normal termination
+			}
+		} catch (IOException e) {
+			System.out.println("===IOException! See stack trace below===");
+			e.printStackTrace();
+			System.exit(SKIP_RETURN_CODE);
+		}
+	}
+
+	/**
+	 * 
+	 * @param fileName
+	 * @return boolean good or bad commit
+	 * @throws IOException
+	 */
+	public static boolean isVulnerable(String fileName) throws IOException {
+		boolean isVulnerable = false;
+		// Open the file that is the first
+		// command line parameter
+		FileInputStream fstream = new FileInputStream(fileName);
+		// Get the object of DataInputStream
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		StringBuffer stringBuffer = new StringBuffer();
+		// Read file line by line, removing newlines
+		while ((strLine = br.readLine()) != null) {
+			stringBuffer.append(strLine.trim());
+		}
+		// Close the input stream
+		in.close();
+		/**
+		 * if the file contains this code, then it's vulnerable
+		 */
+		if (/* initially had this, but took it out 
+		has(stringBuffer, "" + //
+				"{" + // context
+				"char errstr[MAX_STRING_LEN];" + // wrong
+				"apr_size_t len, errstrlen;" + //
+				"apr_file_t *logf = NULL;" + //
+				"const char *referer;" + //
+				"" //
+		) && */ has(stringBuffer, "" + //
+				"}" + // context
+				"errstrlen = len;" + // context
+				"len += apr_vsnprintf(errstr + len, MAX_STRING_LEN - len, fmt, args);" + // wrong
+				"if (r && (referer = apr_table_get(r->headers_in, \"Referer\"))) {" + // wrong
+				"len += apr_snprintf(errstr + len, MAX_STRING_LEN - len," + // context
+				"\", referer: %s\", referer);" + // wrong
+				""//
+		)) {
+			isVulnerable = true;
+		} else {
+			isVulnerable = false; // no such context is found
+		}
+		return isVulnerable;
+	}
+
+	private static boolean has(StringBuffer stringBuffer, String str) {
+		return stringBuffer.indexOf(str) > 0;
+	}
+}
