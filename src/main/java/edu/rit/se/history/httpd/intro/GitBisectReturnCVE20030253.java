723efc6516b7453f1344486f9d0c2b66877f741e
Andy Meneely <andy.meneely@gmail.com>
2012-09-17 15:35:12 -0400

    CVE-2003-0253 bisected

---
+++ src/main/java/edu/rit/se/history/httpd/intro/GitBisectReturnCVE20030253.java
@@ -0,0 +1,98 @@
+package edu.rit.se.history.httpd.intro;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * CVE-2003-0253: server/mpm/prefork/prefork.c
+ * 
+ * Fix commit: 9bc02aba1972e6a7b2e691008cdac6618ef2f516
+ * 
+ * Origin commit: 72ad541beb16444b3146ddf62807c13fe0ebb6d9
+ * 
+ * <pre>
+ *  git bisect start 9bc02aba1972e6a7b2e691008cdac6618ef2f516^ 72ad541beb16444b3146ddf62807c13fe0ebb6d9 -- server/mpm/prefork/prefork.c
+ *  git bisect run java -cp ../httpd-history/src/main/java/ edu.rit.se.history.httpd.intro.GitBisectReturnCVE20030253
+ * </pre>
+ * 
+ * @author Andy Meneely
+ * 
+ */
+public class GitBisectReturnCVE20030253 {
+
+	private static final int GOOD_RETURN_CODE = 0;
+	private static final int BAD_RETURN_CODE = 1;
+	private static final int SKIP_RETURN_CODE = 125;
+
+	private static final String CVE = "CVE-2003-0253";
+	private static final String FILE = "server/mpm/prefork/prefork.c";
+
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			System.out.println("No arguments allowed to this script!");
+			System.exit(SKIP_RETURN_CODE);
+		}
+		System.out.println("===Bisect check for " + CVE + ", " + FILE + "===");
+		try {
+			if (isVulnerable(FILE)) {
+				System.out.println("===VULNERABLE===");
+				System.exit(BAD_RETURN_CODE); // vulnerable --> commit was "bad" --> abnormal termination
+			} else {
+				System.out.println("===NEUTRAL===");
+				System.exit(GOOD_RETURN_CODE); // neutral --> commit was "good" --> normal termination
+			}
+		} catch (IOException e) {
+			System.out.println("===IOException! See stack trace below===");
+			e.printStackTrace();
+			System.exit(SKIP_RETURN_CODE);
+		}
+	}
+
+	/**
+	 * 
+	 * @param fileName
+	 * @return boolean good or bad commit
+	 * @throws IOException
+	 */
+	public static boolean isVulnerable(String fileName) throws IOException {
+		boolean isVulnerable = false;
+		// Open the file that is the first
+		// command line parameter
+		FileInputStream fstream = new FileInputStream(fileName);
+		// Get the object of DataInputStream
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		StringBuffer stringBuffer = new StringBuffer();
+		// Read file line by line, removing newlines
+		while ((strLine = br.readLine()) != null) {
+			stringBuffer.append(strLine.trim());
+		}
+		// Close the input stream
+		in.close();
+		/**
+		 * if the file contains this code, then it's vulnerable
+		 * 
+		 */
+		if (/*has(stringBuffer, "" + //
+				" for (;;) {" + // wrong code
+				"status = listensocks[offset].accept_func(&csd," + // context
+				"&listensocks[offset], ptrans);" + // context
+				" if (status == APR_SUCCESS) {" + // context
+				"")
+				&&*/ has(stringBuffer,
+						"if (APR_STATUS_IS_EINTR(status) && one_process && shutdown_pending) {")) {
+			isVulnerable = true;
+		} else {
+			isVulnerable = false; // no such context is found, must have pre-dated the vulnerability
+		}
+		return isVulnerable;
+	}
+
+	private static boolean has(StringBuffer stringBuffer, String str) {
+		return stringBuffer.indexOf(str) > 0;
+	}
+}
