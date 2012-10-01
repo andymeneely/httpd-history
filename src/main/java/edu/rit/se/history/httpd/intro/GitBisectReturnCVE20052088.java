9a8c82db2f135734fc66bad14e5cb460dd670650
Andy Meneely <andy.meneely@gmail.com>
2012-09-30 23:42:36 -0400

    re-bisecting CVE-2005-2088
    also is the first example of a vulnerability that cross a renamed file

--- src/main/java/edu/rit/se/history/httpd/intro/GitBisectReturnCVE20052088.java
+++ src/main/java/edu/rit/se/history/httpd/intro/GitBisectReturnCVE20052088.java
@@ -2,40 +2,62 @@ package edu.rit.se.history.httpd.intro;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
+ * CVE-2005-2088: modules/proxy/mod_proxy_http.c (formerly modules/proxy/proxy_http.c)
  * 
- */
-
-/**
- * CVE-2005-2088: /modules/proxy/mod_proxy_http.c
- * @author harsha
- * @version v2
+ * Fix commit: 01fce1eded3d0e78951125b4f26ff9e337907543
+ * 
+ * Origin commit: 5d855a48777529f38b148c19c021a01685677f79
+ * 
+ * Note: since there was a rename prior to fixing, we are doing a full-on checkout of the tree, not specific
+ * to the branch
+ * 
+ * <pre>
+ *  git bisect start d107d7d6af0fa59166ea547a09a3925fb74e498e^ 5d855a48777529f38b148c19c021a01685677f79^ 
+ *  git bisect run java -cp ../httpd-history/src/main/java/ edu.rit.se.history.httpd.intro.GitBisectReturnCVE20052088
+ * </pre>
+ * 
+ * @author Andy Meneely
  * 
  */
 public class GitBisectReturnCVE20052088 {
 
-	/**
-	 * @param args
-	 */
+	private static final int GOOD_RETURN_CODE = 0;
+	private static final int BAD_RETURN_CODE = 1;
+	private static final int SKIP_RETURN_CODE = 125;
+
+	private static final String CVE = "CVE-2004-0885";
+	private static final String FILE = "modules/proxy/mod_proxy_http.c";
+	private static final String PRE_RENAME_FILE = "modules/proxy/proxy_http.c";
+
 	public static void main(String[] args) {
-		boolean commitStatus = false;
-		//use specific CVE identifier here..
-		System.out.println("Bisecting for <CVE-2005-2088>");
+		if (args.length > 0) {
+			System.out.println("No arguments allowed to this script!");
+			System.exit(SKIP_RETURN_CODE);
+		}
+		String file = FILE; // file from the fix
+		if (!new File(file).exists()){
+			System.out.println(file + " does not exist, using previous file: " + PRE_RENAME_FILE);
+			file = PRE_RENAME_FILE; // if the checkout was before the rename
+		}
+		System.out.println("===Bisect check for " + CVE + ", " + FILE + "===");
 		try {
-			//args[0] is the full path to the file that was fixed
-			commitStatus = bisectBadOrGood(args[0]);
-			System.out.println("CommitStatus::" + commitStatus);
-			if(commitStatus==true) {
-				System.exit(0);
+			if (isVulnerable(file)) {
+				System.out.println("===VULNERABLE===");
+				System.exit(BAD_RETURN_CODE); // vulnerable --> commit was "bad" --> abnormal termination
 			} else {
-				System.exit(1);
+				System.out.println("===NEUTRAL===");
+				System.exit(GOOD_RETURN_CODE); // neutral --> commit was "good" --> normal termination
 			}
-		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+			System.out.println("===IOException! See stack trace below===");
 			e.printStackTrace();
+			System.exit(SKIP_RETURN_CODE);
 		}
 	}
 
@@ -43,65 +65,45 @@ public class GitBisectReturnCVE20052088 {
 	 * 
 	 * @param fileName
 	 * @return boolean good or bad commit
-	 * @throws FileNotFoundException
+	 * @throws IOException
 	 */
-	public static boolean bisectBadOrGood(String fileName)
-			throws FileNotFoundException {
-		System.out.println("entered bisectBadOrGood");
-		boolean goodCommit = false;
-		try {
-			// Open the file that is the first
-			// command line parameter
-			FileInputStream fstream = new FileInputStream(fileName);
-			// Get the object of DataInputStream
-			DataInputStream in = new DataInputStream(fstream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String strLine;
-			StringBuffer stringBuffer = new StringBuffer();
-			// Read File Line By Line
-			while ((strLine = br.readLine()) != null) {
-				stringBuffer.append(strLine);				
-			}
-			// Close the input stream
-			in.close();
-			//System.out.println(stringBuffer);
-			/**
-			 * if checks for the good commit, else vice versa
-			 * check for the context here, context is determined by what the 
-			 * researcher deems important to the fix
-			 * additional commented lines can be uncommented for checking other 
-			 * contexts that seem fit
-			 */
-			System.out.println(stringBuffer.indexOf("apr_status_t ap_proxy_http_process_response(apr_pool_t * p, request_rec *r,"));
-			System.out.println(stringBuffer.indexOf("if (apr_table_get(r->headers_out, \"Transfer-Encoding\")"));
-			System.out.println(stringBuffer.indexOf("&& apr_table_get(r->headers_out, \"Content-Length\"))"));
-			System.out.println(stringBuffer.indexOf("backend->close += ap_proxy_liststr(apr_table_get(r->headers_out,"));
-			System.out.println(stringBuffer.indexOf("ap_proxy_clear_connection(p, r->headers_out);"));
-			if(stringBuffer.indexOf("apr_status_t ap_proxy_http_process_response(apr_pool_t * p, request_rec *r,")>0
-					&&stringBuffer.indexOf("if (apr_table_get(r->headers_out, \"Transfer-Encoding\")")>0
-					&&stringBuffer.indexOf("&& apr_table_get(r->headers_out, \"Content-Length\")) {")>0
-					&&stringBuffer.indexOf("proxy: server %s returned Transfer-Encoding and Content-Length")>0
-					&&stringBuffer.indexOf("backend->close += ap_proxy_liststr(apr_table_get(r->headers_out,")>0
-					) {
-				System.out.println("Good Commit Context Met, commit was good");
-				goodCommit = true;
-			} else if(stringBuffer.indexOf("apr_status_t ap_proxy_http_process_response(apr_pool_t * p, request_rec *r,")>0
-					&&stringBuffer.indexOf("if (apr_table_get(r->headers_out, \"Transfer-Encoding\")")<0
-					&&stringBuffer.indexOf("&& apr_table_get(r->headers_out, \"Content-Length\"))")<0
-					&&stringBuffer.indexOf("backend->close += ap_proxy_liststr(apr_table_get(r->headers_out,")>0
-					&&stringBuffer.indexOf("ap_proxy_clear_connection(p, r->headers_out);")>0
-					) {
-				System.out.println("Context for good commit not found, bad commit");
-				goodCommit = false;
-			} else {
-				goodCommit = true;
-			}
-		} catch (Exception e) {
-			// Catch exception if any
-			System.err.println("Error: " + e.getMessage());
+	public static boolean isVulnerable(String fileName) throws IOException {
+		boolean isVulnerable = false;
+		// Open the file that is the first
+		// command line parameter
+		FileInputStream fstream = new FileInputStream(fileName);
+		// Get the object of DataInputStream
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		StringBuffer sb = new StringBuffer();
+		// Read file line by line, removing newlines
+		while ((strLine = br.readLine()) != null) {
+			sb.append(strLine.trim());
 		}
-		System.out.println("exiting bisectBadOrGood");
-		return goodCommit;
-		
+		// Close the input stream
+		in.close();
+		/**
+		 * if the file contains this code, then it's vulnerable
+		 * 
+		 */
+		if (has(sb, //
+				"save_table);" + // context
+						"}" + // context
+						"/* strip connection listed hop-by-hop headers from response */" + // vuln fix was
+																							// before here
+						"")) {
+			isVulnerable = true;
+		} else {
+			isVulnerable = false; // no such context is found, must have pre-dated the vulnerability
+		}
+		return isVulnerable;
+	}
+
+	private static boolean has(StringBuffer stringBuffer, String str) {
+		boolean has = stringBuffer.indexOf(str) > 0;
+		if (!has)
+			System.out.println("\tContext not found: " + str);
+		return has;
 	}
 }
