fb47e1e01c44840a23a873620ccf2872bb5944ad
bspates <bxs4361@g.rit.edu>
2013-02-03 00:37:15 -0500

    Added LOC data parsing to DB rebuild and GitLogParser

--- src/main/java/edu/rit/se/history/httpd/parse/GitLogParser.java
+++ src/main/java/edu/rit/se/history/httpd/parse/GitLogParser.java
@@ -1,6 +1,10 @@
 package edu.rit.se.history.httpd.parse;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.text.ParseException;
@@ -10,6 +14,9 @@ import java.util.List;
 import java.util.Scanner;
 
 import org.chaoticbits.devactivity.DBUtil;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GitLogParser {
 	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(GitLogParser.class);
@@ -17,6 +24,7 @@ public class GitLogParser {
 
 	public GitLogParser() {
 		format = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy +SSSS");
+		
 	}
 
 	/**
@@ -30,11 +38,12 @@ public class GitLogParser {
 	 * @param gitLog
 	 * @throws Exception
 	 */
-	public void parse(DBUtil dbUtil, File gitLog) throws Exception {
+	public void parse(DBUtil dbUtil, File gitLog, File gitLogLoc) throws Exception {
+		JSONObject gitLogLocData = parseJSONFromFile(gitLogLoc); 
 		Connection conn = dbUtil.getConnection();
 		PreparedStatement ps = conn.prepareStatement("INSERT INTO GitLog(Commit, AuthorName, AuthorEmail, "
 				+ "AuthorDate, Parent, Subject, Body, NumSignedOffBys) " + "VALUES (?,?,?,?,?,?,?,?)");
-		PreparedStatement ps2 = conn.prepareStatement("INSERT INTO GitLogFiles(Commit,Filepath) " + "VALUES (?,?)");
+		PreparedStatement ps2 = conn.prepareStatement("INSERT INTO GitLogFiles(Commit,Filepath,LinesOfCode) " + "VALUES (?,?,?)");
 		Scanner scanner = new Scanner(gitLog);
 		log.debug("Scanning the log...");
 		scanner.nextLine();
@@ -61,7 +70,7 @@ public class GitLogParser {
 					numSignedOffBys++;
 				nextLine = scanner.nextLine();
 			}
-			parseFileChanges(conn, commit, bodyLines, ps2);
+			parseFileChanges(conn, commit, bodyLines, ps2, gitLogLocData);
 			int i = 1;
 			ps.setString(i++, commit);
 			ps.setString(i++, authorName);
@@ -81,7 +90,7 @@ public class GitLogParser {
 		conn.close();
 	}
 
-	private static void parseFileChanges(Connection conn, String commit, List<String> bodyLines, PreparedStatement ps2)
+	private static void parseFileChanges(Connection conn, String commit, List<String> bodyLines, PreparedStatement ps2, JSONObject gitLogLocData)
 			throws Exception {
 		if (bodyLines.size() < 3) // no changes to files - you can do this by changing file permissions in git
 			return;
@@ -91,11 +100,79 @@ public class GitLogParser {
 			String file = fileChange.substring(0, pipeLoc).trim();
 			if (file.startsWith(".../")) // remove the .../tests filepath weirdness
 				file = file.substring(4);
+			
+			Integer loc = parseGitLogLoc(file, commit, gitLogLocData);
+			
 			ps2.setString(1, commit);
 			ps2.setString(2, file);
+			ps2.setInt(3, loc);
+			
 			ps2.addBatch();
 		}
 	}
+	
+	/* 
+	 * Begin Methods contributed by Brian Spates
+	 * Method matches filepath and commit to said revision's SLOC and returns the SLOC value
+	 */
+	private static int parseGitLogLoc(String filepath, String commit, JSONObject gitLogLocData)
+	{
+		JSONArray fileArr;
+		try {
+			fileArr = gitLogLocData.getJSONArray(filepath);
+		
+			for(int i = 0; i < fileArr.length(); i++)
+			{
+				if(fileArr.getString(i).equals(commit))
+				{
+					return fileArr.getInt(i+1);
+				}
+			}
+			return 0;
+		} catch (JSONException e) {
+			log.info(e.getMessage());
+			return 0;
+		}
+	}
+	
+	// Generic method to retrieve JSONObject that represents a whole JSON file
+	private JSONObject parseJSONFromFile(File locFile)
+	{
+		String logs;
+		try{
+			FileInputStream fis = new FileInputStream(locFile);
+			InputStreamReader in = new InputStreamReader(fis);
+			BufferedReader br = new BufferedReader(in);
+			StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+            }
+            fis.close();
+            in.close();
+            br.close();
+            logs = sb.toString();
+            
+            JSONObject gitLogLocData = new JSONObject(logs);
+            
+            return gitLogLocData;
+	
+		}
+		catch(JSONException e)
+		{
+			log.error(e.getMessage());
+			return null;
+		}
+		catch(IOException e)
+		{
+			log.error(e.getMessage());
+			return null;
+		}
+		
+	}
+	/* 
+	 * END methods contributed by Brian Spates
+	 */
 
 	public java.util.Date parseDate(String testStr) throws ParseException {
 		if (testStr.startsWith("Sun Mar 12 02")) // stupid daylight savings bug
@@ -106,4 +183,5 @@ public class GitLogParser {
 			testStr = "Sun Mar 10 01:00:00 2002 +0000";
 		return format.parse(testStr);
 	}
+	
 }
