4742513df0d72bdf6da22b14a903102b7d238805
Andy Meneely <andy.meneely@gmail.com>
2013-02-07 15:41:00 -0500

    Fixed a join bug

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -5,5 +5,5 @@ CREATE TABLE VulnIntroAll AS
 			r.LinesDeleted + r.LinesInserted as TotalChurn,
 			IF(r.LinesDeleted=0, 0, r.LinesDeletedOther/r.LinesDeleted) as PercIntChurn,
 			IF(c2g.CommitIntroduced IS NOT NULL, "Yes", "No") VulnIntro
-	FROM RepoLog r LEFT OUTER JOIN CVEToGit c2g ON r.Commit=c2g.CommitIntroduced
+	FROM RepoLog r LEFT OUTER JOIN CVEToGit c2g ON (r.Commit=c2g.CommitIntroduced AND r.Filepath=c2g.Filepath)
 ;
