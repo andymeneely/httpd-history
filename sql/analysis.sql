9193ecc5f4e346f79790a6c44f4a9deebec1ee35
Andy Meneely <andy@se.rit.edu>
2013-02-12 17:00:14 -0500

    Use GitLogFiles for analysis table

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -7,7 +7,7 @@ CREATE TABLE VulnIntroAll AS
 			(r.LinesDeleted + r.LinesInserted)/r.LinesOfCode as RelativeChurn,
 			r.LinesDeletedOther/r.LinesDeleted as PercIntChurn,
 			IF(c2g.CommitIntroduced IS NOT NULL, "Yes", "No") VulnIntro
-	FROM RepoLog r LEFT OUTER JOIN CVEToGit c2g ON (r.Commit=c2g.CommitIntroduced AND r.Filepath=c2g.Filepath)
+	FROM GitLogFiles r LEFT OUTER JOIN CVEToGit c2g ON (r.Commit=c2g.CommitIntroduced AND r.Filepath=c2g.Filepath)
 ;
 
 CREATE TABLE VulnIntroByCommit AS
