582c18c4aa8573d19720f040c04e5668ee983490
Andy Meneely <andy@se.rit.edu>
2013-02-12 17:02:28 -0500

    DBVerify update for Alberto's corrections

--- src/main/java/edu/rit/se/history/httpd/dbverify/AllCVEToGitInAnalysis.java
+++ src/main/java/edu/rit/se/history/httpd/dbverify/AllCVEToGitInAnalysis.java
@@ -5,7 +5,7 @@ import org.chaoticbits.devactivity.testutil.dbverify.SQLCountDBVerify;
 public class AllCVEToGitInAnalysis extends SQLCountDBVerify {
 
 	public AllCVEToGitInAnalysis() {
-		super("Only a few known CVEToGit entries failed to join (non-source files, or N/A files)", 14,
+		super("Only a few known CVEToGit entries failed to join (non-source files, or N/A files)", 22,
 				"SELECT COUNT(*) FROM CVEToGit c2g WHERE "
 						+ "c2g.CommitIntroduced NOT IN (SELECT Commit FROM VulnIntroAll WHERE VulnIntro=\"Yes\")");
 	}
