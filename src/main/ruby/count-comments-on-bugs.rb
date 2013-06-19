d1f983e74e7b961056b2795e7e9e7623db2ed95f
Andy Meneely <andy@se.rit.edu>
2013-06-19 08:37:35 -0400

    Adding a script to download and count comments
    Horribly inefficient with 6400 bugs, but hey, it's ok

---
+++ src/main/ruby/count-comments-on-bugs.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#require 'csv'
+require 'mechanize'
+
+a = Mechanize.new
+
+CSV.foreach("bugs-2013-06-18.csv") do | row |
+  bug_id = row[0].to_i
+  table = {}
+  #puts "Downloading bug #{bug_id}..."
+  a.get("https://issues.apache.org/bugzilla/show_bug.cgi?ctype=xml&id=#{bug_id}") do | page |
+    puts "#{bug_id}\t#{page.search("//commentid").size}"
+    File.open("./xml/#{bug_id}.xml","w+"){ |file| file.write(page.body) }
+  end
+end
+
+p "Done!"
