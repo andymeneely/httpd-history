7a4ede0d39a07482caca8c71b84b6b4c641f9f45
Andy Meneely <andy@se.rit.edu>
2013-06-21 10:31:38 -0400

    Tweaking this script, but use query-dadds.rb
    Added a query thing, but query-dadds.rb (next commit) is more advanced
    and customizable

--- src/main/ruby/count-comments-on-bugs.rb
+++ src/main/ruby/count-comments-on-bugs.rb
@@ -5,9 +5,11 @@ require 'trollop'
 opts = Trollop::options do
   banner "Count the number of comments in Bugzlla bugs stored in xml files"
   opt :xmls, "The xml/ directory of Bugzillas", :default => './xml/'
+  opt :min, "The minimum number of comments to show", :default => 0
 end
 
 Trollop::die "xml directory must be a directory" unless Dir.exists?(opts[:xmls])
+Trollop::die "min must be 0 or greater" unless opts[:min]>=0
 
 Dir.chdir(opts[:xmls])
 
@@ -15,6 +17,6 @@ Dir.glob("*.xml") do |file|
   xml = Nokogiri::XML(File.open(file,"r"))
   id = xml.at_xpath("//bug_id").content.to_i
   num_comments = xml.xpath("//commentid").size
-  puts "#{id}\t#{num_comments}"
+  puts "#{id}\t#{num_comments}" if num_comments >= opts[:min]
 end
 
