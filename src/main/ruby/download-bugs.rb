9f6f0b8a637e8e0d0731f73e219b8685b8c8ed04
Andy Meneely <andy@se.rit.edu>
2013-06-24 15:39:28 -0400

    Refactoring to download a small subset of bugs

--- src/main/ruby/download-bugs.rb
+++ src/main/ruby/download-bugs.rb
@@ -5,16 +5,19 @@ require 'ruby-progressbar'
 
 a = Mechanize.new
 
-num_files = File.open("allbugs.csv").readlines.size
+#bugfile = "allbugs.csv"
+bugfile = "dadd-random.txt"
+
+num_files = File.open(bugfile).readlines.size
 
 puts "Downloading #{num_files} files into xml/\nThis could take about #{num_files/20} minutes.\n"
 progress = ProgressBar.create(:starting_at => 0, :total => num_files)
 
-CSV.foreach("allbugs.csv") do | row |
+CSV.foreach(bugfile) do | row |
   bug_id = row[0].to_i
   #print "Downloading bug #{bug_id}..."
   a.get("https://issues.apache.org/bugzilla/show_bug.cgi?ctype=xml&id=#{bug_id}") do | page |
-    File.open("./xml/#{bug_id}.xml","w+"){ |file| file.write(page.body) }
+    File.open("./sample-xmls/#{bug_id}.xml","w+"){ |file| file.write(page.body) }
   end
   progress.increment
 end
