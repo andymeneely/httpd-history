307048c4299c83cced7b58ccc7e977c8cc3e3db3
Andy Meneely <andy@se.rit.edu>
2013-06-24 15:44:06 -0400

    Adding counter to the downloader to order the files properly

--- src/main/ruby/download-bugs.rb
+++ src/main/ruby/download-bugs.rb
@@ -13,11 +13,14 @@ num_files = File.open(bugfile).readlines.size
 puts "Downloading #{num_files} files into xml/\nThis could take about #{num_files/20} minutes.\n"
 progress = ProgressBar.create(:starting_at => 0, :total => num_files)
 
+counter = 0
+
 CSV.foreach(bugfile) do | row |
   bug_id = row[0].to_i
   #print "Downloading bug #{bug_id}..."
   a.get("https://issues.apache.org/bugzilla/show_bug.cgi?ctype=xml&id=#{bug_id}") do | page |
-    File.open("./sample-xmls/#{bug_id}.xml","w+"){ |file| file.write(page.body) }
+    File.open("./sample-xmls/#{'%04d'%counter}-#{bug_id}.xml","w+"){ |file| file.write(page.body) }
+    counter+=1
   end
   progress.increment
 end
