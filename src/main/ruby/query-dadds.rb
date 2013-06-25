0fa9830d2c1fa0f368145fb9a82f9e37716ba547
Andy Meneely <andy@se.rit.edu>
2013-06-25 14:03:08 -0400

    Adding option to limit bug list

--- src/main/ruby/query-dadds.rb
+++ src/main/ruby/query-dadds.rb
@@ -3,20 +3,33 @@ require 'nokogiri'
 require 'trollop'
 require 'base64'
 
+pwd = Dir.getwd
+
 opts = Trollop::options do
   banner "Count the number of comments in Bugzlla bugs stored in xml files"
   opt :xmls, "The xml/ directory of Bugzillas", :default => './xml/'
+  opt :bugs, "A file with the bug IDs to collect", :default => ''
   opt :min_comments, "The minimum number of comments to show", :default => 0
   opt :min_commentors, "The minimum number of unique commentators on the bug", :default => 0
 end
 
 Trollop::die "xml directory must be a directory" unless Dir.exists?(opts[:xmls])
+Trollop::die "bugs file not found!" unless File.exists?(opts[:bugs]) || opts[:bugs].eql?('')
 Trollop::die "min-comments must be 0 or greater" unless opts[:min_comments]>=0
 Trollop::die "min-commentors must be 0 or greater" unless opts[:min_commentors]>=0
 
+if opts[:bugs].nil? || opts[:bugs].eql?('')
+  Dir.chdir(opts[:xmls]); 
+  files = Dir.glob("*.xml")
+  Dir.chdir(pwd)
+else
+  files = File.open(opts[:bugs]).readlines.collect {|f| f.strip + ".xml" }
+  puts files
+end
+
 Dir.chdir(opts[:xmls])
 
-Dir.glob("*.xml") do |file|
+files.each do |file|
   xml = Nokogiri::XML(File.open(file,"r"))
 
   # Pull the comment IDs and commentors
