14724e8c0ced6b7d814bddeaf0a3e4791424e35f
Andy Meneely <andy@se.rit.edu>
2013-07-01 11:07:37 -0400

    Refactoring for upcoming change
    Organizing the code a bit better so we can handle the duplicates

--- src/main/ruby/query-dadds.rb
+++ src/main/ruby/query-dadds.rb
@@ -9,14 +9,10 @@ opts = Trollop::options do
   banner "Count the number of comments in Bugzlla bugs stored in xml files"
   opt :xmls, "The xml/ directory of Bugzillas", :default => './xml/'
   opt :bugs, "A file with the bug IDs to collect", :default => ''
-  opt :min_comments, "The minimum number of comments to show", :default => 0
-  opt :min_commentors, "The minimum number of unique commentators on the bug", :default => 0
 end
 
 Trollop::die "xml directory must be a directory" unless Dir.exists?(opts[:xmls])
 Trollop::die "bugs file not found!" unless File.exists?(opts[:bugs]) || opts[:bugs].eql?('')
-Trollop::die "min-comments must be 0 or greater" unless opts[:min_comments]>=0
-Trollop::die "min-commentors must be 0 or greater" unless opts[:min_commentors]>=0
 
 if opts[:bugs].nil? || opts[:bugs].eql?('')
   Dir.chdir(opts[:xmls]); 
@@ -27,29 +23,11 @@ else
   puts files
 end
 
-Dir.chdir(opts[:xmls])
-
-files.each do |file|
+def read_bug(file)
   xml = Nokogiri::XML(File.open(file,"r"))
-
-  # Pull the comment IDs and commentors
-  comment_ids = xml.xpath("//commentid")
-  commentor_nodes = xml.xpath("//who")
-  commentors = []
-  commentor_nodes.each { |c| commentors << c.content }
-
-  # Count the number of comments not made by the reporter
-  non_reporter_comments = 0
-  reporter = xml.at_xpath("//reporter").content
-  commentor_nodes.each { |c| non_reporter_comments+=1 if !reporter.eql?(c.content) }
-
-  commentors.uniq!
-
-  next if commentors.size < opts[:min_commentors] || comment_ids.size < opts[:min_comments]
 
   # Query the XML for stuff
   id = xml.at_xpath("//bug_id").content.to_i
-
   patches = xml.xpath("//attachment[@ispatch=1]")
   votes = xml.at_xpath("//votes").content.to_i
   priority = xml.at_xpath("//priority").content
@@ -57,7 +35,18 @@ files.each do |file|
   status = xml.at_xpath("//bug_status").content
   resolution = xml.at_xpath("//resolution").content
   comments = xml.xpath("//thetext")
+  comment_ids = xml.xpath("//commentid")
+  commentor_nodes = xml.xpath("//who")
+  reporter = xml.at_xpath("//reporter").content
+  
+  # Pull the commentors
+  commentors = []
+  commentor_nodes.each { |c| commentors << c.content }
+  commentors.uniq!
 
+  # Count the number of comments not made by the reporter
+  non_reporter_comments = 0
+  commentor_nodes.each { |c| non_reporter_comments+=1 if !reporter.eql?(c.content) }
 
   # Count the number of files for each patch
   patch_files = []
@@ -81,5 +70,10 @@ files.each do |file|
 
 end
 
-puts "ID\tCommments\tCommentors\tVotes\tPatches\tFiles in Patches\tReplies\tNon-Reporter Comments\tMention RFC?\tPriority\tSeverity\tStatus\tResolution"
+Dir.chdir(opts[:xmls]) do 
+  files.each do |file|
+    read_bug file
+  end
+end
 
+puts "ID\tCommments\tCommentors\tVotes\tPatches\tFiles in Patches\tReplies\tNon-Reporter Comments\tMention RFC?\tPriority\tSeverity\tStatus\tResolution"
