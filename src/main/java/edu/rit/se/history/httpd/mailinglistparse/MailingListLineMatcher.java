763e4dce0c2e4c854d8a645e66305a28c236300c
Felivel Camilo <fdc7162@rit.edu>
2014-03-03 15:50:06 -0500

    working on saving the retrived commits ids to the database.

--- src/main/java/edu/rit/se/history/httpd/mailinglistparse/MailingListLineMatcher.java
+++ src/main/java/edu/rit/se/history/httpd/mailinglistparse/MailingListLineMatcher.java
@@ -5,16 +5,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.UnknownHostException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.io.FileUtils;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
 
 public class MailingListLineMatcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MailingListLineMatcher lineMatcher = new MailingListLineMatcher();
+		lineMatcher.setUpDB();
 		lineMatcher.readFile("C:/mailinglist/VulnIntro.csv");
 	}
+
+	private DBCollection emailCollection;
 
 	private void readFile(String path) {
 
@@ -43,22 +56,23 @@ public class MailingListLineMatcher {
 		File file = new File("C:/mailinglist/downloads/" + filename + ".txt");
 		int startLineNumber = Integer.valueOf(lineNumber);
 		try {
-			
+
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line;
-			
+
 			for (int i = 1; i < startLineNumber; i++) {
 				br.readLine();
 			}
-			
+
 			while ((line = br.readLine()) != null) {
 				if (line.toLowerCase().startsWith("message-id:")) {
-					System.out.println(comitId + "," + line.split(" ")[1]);
+					String emailID = line.split(" ")[1];
+					System.out.println(comitId + "," + emailID);
+					saveToDB(emailID,comitId);
 					break;
 				}
 			}
 			br.close();
-			
 
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found");
@@ -69,4 +83,23 @@ public class MailingListLineMatcher {
 
 	}
 
+	private void setUpDB() {
+		// mongoDB set up
+		MongoClient mongo;
+		try {
+			mongo = new MongoClient("localhost", 27017);
+			DB db = mongo.getDB("mailinglist");
+			this.emailCollection = db.getCollection("email");
+		} catch (UnknownHostException e) {
+			System.out.println("Error: There was an while setting up the database :" + e.getMessage());
+		}
+	}
+
+	private void saveToDB(String emailID, String commitId){
+		BasicDBObject query = new BasicDBObject();
+	    query.put("messageID", emailID);
+	    
+		DBObject email = emailCollection.findOne(query);
+		System.out.println(email);
+	}
 }
